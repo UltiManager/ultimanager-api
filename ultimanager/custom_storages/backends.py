bcc823e66af580954e4b65e679f9415bfef01745
Chathan Driehuys <chathan@driehuys.com>
2019-05-02 21:23:15 -0400

    Another attempted fix for S3 storage.

--- ultimanager/custom_storages/backends.py
+++ ultimanager/custom_storages/backends.py
@@ -1,26 +1,50 @@
 """Custom storage backends.
 """
+import os
+
 from django.conf import settings
 from django.contrib.staticfiles.storage import ManifestFilesMixin
-from storages.backends.s3boto3 import S3Boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage, SpooledTemporaryFile
 
 
 class PatchedS3StaticStorage(S3Boto3Storage):
-    def _save(self, name, content):
-        if (
-            hasattr(content, "seek")
-            and hasattr(content, "seekable")
-            and content.seekable()
-        ):
-            content.seek(0)
-        return super()._save(name, content)
+    def _save_content(self, obj, content, parameters):
+        """
+        We create a clone of the content file as when this is passed to
+        boto3 it wrongly closes the file upon upload where as the
+        storage backend expects it to still be open.
+
+        See Also:
+            https://github.com/jschneier/django-storages/issues/382#issuecomment-377174808
+        """
+        # Seek our content back to the start
+        content.seek(0, os.SEEK_SET)
+
+        # Create a temporary file that will write to disk after a
+        # specified size
+        content_autoclose = SpooledTemporaryFile()
+
+        # Write our original content into our copy that will be closed
+        # by boto3
+        content_autoclose.write(content.read())
+
+        # Upload the object which will auto close the content_autoclose
+        # instance
+        super(PatchedS3StaticStorage, self)._save_content(
+            obj, content_autoclose, parameters
+        )
+
+        # Cleanup if this is fixed upstream our duplicate should always
+        # close
+        if not content_autoclose.closed:
+            content_autoclose.close()
 
 
 class CachedS3Storage(ManifestFilesMixin, PatchedS3StaticStorage):
     pass
 
 
-class MediaStorage(CachedS3Storage):
+class MediaStorage(S3Boto3Storage):
     """
     Storage class for media files.
 
@@ -33,7 +57,7 @@ class MediaStorage(CachedS3Storage):
     location = "media"
 
 
-class StaticStorage(ManifestFilesMixin, S3Boto3Storage):
+class StaticStorage(CachedS3Storage):
     """
     Storage class for static files.
 
